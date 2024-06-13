e8f0bab80b69206472734047964d2fd451fa218e
AlfaGeeks <admin@alfageeks.in>
2024-06-13 13:47:13 +0530

    Initial commit
    Signed-off-by: AlfaGeeks <admin@alfageeks.in>

---
+++ app/src/main/java/in/alfageeks/rangeslider/demo/MainActivity.kt
@@ -0,0 +1,47 @@
+package `in`.alfageeks.rangeslider.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import `in`.alfageeks.rangeslider.demo.ui.theme.RangeSliderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RangeSliderTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RangeSliderTheme {
+        Greeting("Android")
+    }
+}
