8eb1317313d5e496111c35e6ffe4afeac491b4aa
AlfaGeeks <admin@alfageeks.in>
2024-06-28 11:06:24 +0530

    implemented maven publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
--- range-slider/build.gradle.kts
+++ range-slider/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
+    id("com.vanniktech.maven.publish")
 }
 
 android {
@@ -34,10 +38,29 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    compileOnly(platform(libs.androidx.compose.bom))
+    compileOnly(libs.androidx.ui)
+    compileOnly(libs.androidx.ui.graphics)
+    compileOnly(libs.androidx.ui.tooling.preview)
+    compileOnly(libs.androidx.material3)
     debugImplementation(libs.androidx.ui.tooling)
+}
+
+mavenPublishing {
+    configure(
+        AndroidSingleVariantLibrary(
+            // the published variant
+            variant = "release",
+            // whether to publish a sources jar
+            sourcesJar = false,
+            // whether to publish a javadoc jar
+            publishJavadocJar = false,
+        )
+    )
+//    publishToMavenCentral(SonatypeHost.DEFAULT)
+    // or when publishing to https://s01.oss.sonatype.org
+//    publishToMavenCentral(SonatypeHost.S01)
+    // or when publishing to https://central.sonatype.com/
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
